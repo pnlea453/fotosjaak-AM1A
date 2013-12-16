a1166fd4da35b88804c6457ec8a8dd5b8ed4d2dc
Hoessein Ait Ichou <m_ocro_amazigh@hotmail.com>
2013-12-16 15:56:15 +0100

    Format gewijzigd van register

--- register_form.php
+++ register_form.php
@@ -67,54 +67,16 @@
 						<input type="radio" name="sex" value="female">Vrouw
 					</td>
 				</tr>
-				<tr>
-					<td>burgerlijke staat</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="radio" name="marital_status" value="married">Getrouwd<br>
-						<input type="radio" name="marital_status" value="unmarried">Ongehuwd
-						<input type="radio" name="marital_status" value="living together">Samen wonend
-					</td>
-				</tr>
+				
 				<tr>
 				</tr>
 				
-				<tr>
-					<td>favoriete gamesoort
-				</tr>
-				<tr>
-					<td>
-					<select name="favorite_game_genres">
-							<option value="1">FPS</option>
-							<option value="2">RPG</option>
-							<option value="3">MMO</option>
-							<option value="4">MMORPG</option>
-					</td>
-				</tr>
-				
-				<tr>
-					<td>favoriete spel</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="text" name="favorite_game" />
-					</td>
-				</tr>
 				<tr>
 					<td>e-mail</td>
 				</tr>
 				<tr>
 					<td>
 						<input type="e-mail" name="e-mail">
-					</td>
-				</tr>
-				<tr>
-					<td>password</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="password" name="password" />
 					</td>
 				</tr>
 				
