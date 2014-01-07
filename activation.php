5c063bd5d91fa182b047fa940d54172f9f8fbfee
Hoessein Ait Ichou <m_ocro_amazigh@hotmail.com>
2014-01-07 10:54:20 +0100

    activation

--- activation.php
+++ activation.php
@@ -1,5 +1,21 @@
-<?php
-
-echo "hallo welkom op de registratiepagina";
-
-?>
+<p>Welkom op de site. Uw account wordt geactiveerd nadat u een nieuw wachtwoord
+	heeft gekozen. </p>
+	<form action='' method='post'>
+		<table>
+			<tr>
+				<td>nieuw wachtwoord (maximaal 12 tekens)</td>
+				<td><input type='text'name'password' size='12' maxlength='12'/></td>
+			</tr>
+			<tr>
+				<td>nieuw wachtwoord (check)</td>
+				<td><input type='text' name='password-check' size='12' maxlength='12'/></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td><input type='submit' value='vestuur'/></td>
+			</tr>
+		</table>
+		
+		
+		
+	</from>
